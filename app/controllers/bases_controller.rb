54ef97b95e623ed7dea7a80b9c75d5e0e15b4558
Joseph Bartlow <joebartlow@gmail.com>
2012-09-16 22:35:10 -0600

    added an index and show to the base controller

--- app/controllers/bases_controller.rb
+++ app/controllers/bases_controller.rb
@@ -5,11 +5,32 @@ class BasesController < ApplicationController
         :subtitle => params[:subtitle],
         :page_id => params[:page_id]
     )
+
     if params[:content_text]
-      content.content_texts.create(
-          :body_text => params[:content_text]
-      )
+      content_array = JSON.parse(params[:content_text])
+      content_array.each do |text|
+        content.content_texts.create(
+            :body_text => text['text']
+        )
+      end
     end
+
+    if params[:content_image]
+      image_array = JSON.parse(params[:content_image])
+      image_array.each do |image|
+        content.content_images.create(
+            :image_uri => image['image_uri']
+        )
+      end
+    end
+    render :nothing => true
+  end
+  def index
+    @content = Content.first
+    render :nothing => true
+  end
+  def show
+    @content = Content.find_by_page_id(params[:id])
     render :nothing => true
   end
 end
--- spec/controllers/bases_controller_spec.rb
+++ spec/controllers/bases_controller_spec.rb
@@ -12,11 +12,80 @@ describe BasesController do
       content[:page_id].should == "page0"
     end
     it 'should add a single content text blob to the post' do
-      post :create, :title => 'title1', :subtitle => 'subtitle1', :page_id => 'page01', :content_text => 'some awesome text about stuff'
+      post :create, :title => 'title1', :subtitle => 'subtitle1', :page_id => 'page01', :content_text => [{:text => 'some awesome text about stuff'}].to_json
 
       response.should be_success
       content = Content.last
       content.content_texts.first[:body_text].should == "some awesome text about stuff"
+    end
+    it 'should add multiple content text blobs to the post' do
+      post :create, :title => 'title2', :subtitle => 'subtitle2', :page_id => 'page2', :content_text => [{:text =>'text number 1'}, {:text => 'text number 2'}, {:text =>'somemore text'}].to_json
+
+      response.should be_success
+      content = Content.last
+      content.content_texts.first[:body_text].should == "text number 1"
+      content.content_texts.last[:body_text].should == "somemore text"
+    end
+    it 'should add a single image uri in the post' do
+      post :create, :title => 'title3', :subtitle => 'subtitle3', :page_id => 'page3', :content_image => [{:image_uri => 'uri3'}].to_json
+      response.should be_success
+      content =Content.last
+      content.content_images.first[:image_uri].should == "uri3"
+    end
+    it 'should add multiple image uris to the post' do
+      post :create, :title => 'title4', :subtitle => 'subtitle4', :page_id => 'page4', :content_image => [{:image_uri =>'image4'}, {:image_uri =>'image5'}, {:image_uri =>'image6'}].to_json
+
+      response.should be_success
+      content = Content.last
+      content.content_images.first[:image_uri].should == "image4"
+      content.content_images.last[:image_uri].should == "image6"
+      content.content_images.count.should == 3
+    end
+    it 'should have both image uris and text in the post' do
+      post :create, :title => 'title4', :subtitle => 'subtitle4', :page_id => 'page4', :content_image => [{:image_uri =>'image4'}, {:image_uri =>'image5'}, {:image_uri =>'image6'}].to_json, :content_text => [{:text =>'text number 1'}, {:text => 'text number 2'}, {:text =>'somemore text'}].to_json
+
+      response.should be_success
+      content = Content.last
+      content.content_images.first[:image_uri].should == "image4"
+      content.content_images.last[:image_uri].should == "image6"
+      content.content_images.count.should == 3
+
+      content.content_texts.first[:body_text].should == "text number 1"
+      content.content_texts.last[:body_text].should == "somemore text"
+      content.content_texts.count.should == 3
+    end
+  end
+
+  describe "#index" do
+    it 'should grab content from the database and pass it back' do
+      post :create, :title => 'title4', :subtitle => 'subtitle4', :page_id => 'page4', :content_image => [{:image_uri =>'image4'}, {:image_uri =>'image5'}, {:image_uri =>'image6'}].to_json, :content_text => [{:text =>'text number 1'}, {:text => 'text number 2'}, {:text =>'somemore text'}].to_json
+
+      response.should be_success
+
+      get :index
+      response.should be_success
+      ourcontent = Content.first
+
+      assigns(:content).should_not be_nil
+      assigns(:content).should == ourcontent
+      assigns(:content).content_images.first[:image_uri].should == "image4"
+      assigns(:content).content_texts.first[:body_text].should == "text number 1"
+
+    end
+  end
+
+  describe "#show" do
+    it 'should grab a specific page_id and return that data' do
+      post :create, :title => 'title4', :subtitle => 'subtitle4', :page_id => 'page4', :content_image => [{:image_uri =>'image4'}, {:image_uri =>'image5'}, {:image_uri =>'image6'}].to_json, :content_text => [{:text =>'text number 1'}, {:text => 'text number 2'}, {:text =>'somemore text'}].to_json
+      response.should be_success
+      post :create, :title => 'title5', :subtitle => 'subtitle5', :page_id => 'page5', :content_image => [{:image_uri =>'image7'}, {:image_uri =>'image8'}, {:image_uri =>'image9'}].to_json, :content_text => [{:text =>'text number 11'}, {:text => 'text number 12'}, {:text =>'somemore text 13'}].to_json
+      response.should be_success
+
+      get :show, :id => 'page4'
+      response.should be_success
+      assigns(:content).should_not be_nil
+      assigns(:content).content_images.first[:image_uri].should == "image4"
+      assigns(:content).content_texts.first[:body_text].should == "text number 1"
     end
   end
 end
